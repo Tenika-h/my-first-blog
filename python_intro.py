c5c41f464fd6a67fb1fa7fbc7669a42e373fc9b1
tenikaharris <htenika@yahoo.com>
2025-03-29 13:52:23 -0400

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,19 @@
+print('Hello, Django girls!')
+if 3 > 2:   
+    print('It works!')
+if 5 > 2:
+    print('5 is indeed greater than 2')
+else:
+     print('5 is not greater than 2')
+def hi():
+    print('Hi there!')
+    print('How are you?')  
+hi()
+def hi(name):
+    if name == 'Ola':
+         print('Hi Ola!')
+    elif name == 'Sonja':
+        print('Hi Sonja!')
+    else:
+       print('Hi Sonja!')
+hi("Ola")
